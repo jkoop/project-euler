66ef079d88578527e202b44f0d85bc0f56aae1fa
Joe Koop <joe@joekoop.com>
2021-11-15 10:49:22 -0600

    gave up on xkcd 2529 for now

--- xkcd2529-weirdly-concrete.php
+++ xkcd2529-weirdly-concrete.php
@@ -1,28 +1,31 @@
 <?php
 
+// I've sorta given up on this one.
+
 $n = 4;
 $k = 10;
 
 $nCounter = 0;
 $kCounter = 0;
-$avaliableDirections = ['up','down','left','right'];
+$availableDirections = ['up','down','left','right'];
 $grid = [];
 $position = (object) [
 	'x' => 0,
 	'y' => 0,
 ];
 
-while ($kCounter < $k && count($avaliableDirections) > 0) {
-	setGridCell($grid, $position, '+'); // i was here
+while ($kCounter < $k && count($availableDirections) > 0) {
+	setGridCell($grid, $position, '.'); // i was here
 
 	if ($nCounter + 1 == $n) {
 		setGridCell($grid, $position, 'm'); // marble
 		$kCounter++;
+
 	}
 
 	$nCounter = ($nCounter + 1) % $n;
 
-	$direction = $avaliableDirections[rand(0, count($avaliableDirections) - 1)];
+	$direction = $availableDirections[rand(0, count($availableDirections) - 1)];
 
 	switch ($direction) {
 		case 'up':	$position->y++; break;
@@ -31,14 +34,15 @@ while ($kCounter < $k && count($avaliableDirections) > 0) {
 		case 'right':	$position->x--; break;
 	}
 
-	$avaliableDirections = [];
-	if (getGridCell($grid, $position, 'up') == '.') $avaliableDirections[] = 'up';
-	if (getGridCell($grid, $position, 'down') == '.') $avaliableDirections[] = 'down';
-	if (getGridCell($grid, $position, 'left') == '.') $avaliableDirections[] = 'left';
-	if (getGridCell($grid, $position, 'right') == '.') $avaliableDirections[] = 'right';
+	$availableDirections = [];
+	if (getGridCell($grid, $position, 'up') == ' ') $availableDirections[] = 'up';
+	if (getGridCell($grid, $position, 'down') == ' ') $availableDirections[] = 'down';
+	if (getGridCell($grid, $position, 'left') == ' ') $availableDirections[] = 'left';
+	if (getGridCell($grid, $position, 'right') == ' ') $availableDirections[] = 'right';
+	
+	echoGrid($grid);
+	echo('----' . PHP_EOL);
 }
-
-echoGrid($grid);
 
 function setGridCell(array &$grid, object $position, string $value): string {
 	if (!isset($grid[$position->y])) $grid[$position->y] = [];
@@ -57,22 +61,40 @@ function getGridCell(array &$grid, object $position, ?string $direction = null):
 
 	if (!isset($grid[$position->y])) $grid[$position->y] = [];
 
-	return $grid[$position->y][$position->x] ?? setGridCell($grid, $position, '.');
+	return $grid[$position->y][$position->x] ?? setGridCell($grid, $position, ' ');
 }
 
 function echoGrid(array $grid): void {
+	global $position;
+
 	$gridKeys = array_keys($grid);
 	sort($gridKeys);
 
+	$minGridX = 0;
 	for ($i=0; $i<count($grid); $i++) {
 		$row = $grid[$gridKeys[$i]];
 		$rowKeys = array_keys($row);
 		sort($rowKeys);
 
-		for ($j=0; $j<count($row); $j++) {
-			$cell = $row[$rowKeys[$j]];
+		if ($minGridX > $rowKeys[0]) $minGridX = $rowKeys[0];
+	}
 
-			echo $cell . ', ';
+	for ($i = 0; $i < count($grid); $i++) {
+		$rowNumber = $gridKeys[$i];
+		$row = $grid[$rowNumber];
+
+		$rowKeys = array_keys($row);
+		sort($rowKeys);
+
+		$rowMaxX = $rowKeys[count($rowKeys) - 1];
+
+		for ($j = $minGridX; $j < $rowMaxX; $j++) {
+			if ($rowNumber == 0 && $j == 0) echo "\033[42m"; // green bg
+			if ($rowNumber == $position->y && $j == $position->x) echo "\033[31m"; // red fg
+
+			echo ($row[$j] ?? ' ') . ' ';
+			
+			echo "\033[0m"; // reset
 		}
 
 		echo "\n";
